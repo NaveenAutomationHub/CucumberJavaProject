eb11b740d6ab878ddb3b6373cdb51af570aa589a
Naveen Kumar Dommata <Naveen.Dommata@s2tech.net>
2018-07-10 13:31:34 +0530

    updating tagging concept

---
+++ src/runner/TaggingRunner.java
@@ -0,0 +1,27 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(features={"features/tagging.feature"},
+glue={"stepDefination"},
+format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, 
+dryRun=false,
+monochrome=true,
+tags={"~@End2EndTest" , "~@SmokeTest", "~@RegressionTest"}
+)
+public class TaggingRunner {
+
+	
+	
+}
+
+
+
+// ORed : tags ={"@SmokeTest , @End2EndTest" } execute all test cases tagged as @SmokeTest and @End2EndTest
+
+//ANDed : tags ={"@SmokeTest" , "@End2EndTest" } execute all test cases tagged as @SmokeTest and @End2EndTest
+
+// ~ : is used to skip/ignore the tags(~@SmokeTest)
--- test-outout/report.js
+++ test-outout/report.js
@@ -1,103 +1,29 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("features\\DataDrivenWithMaps.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("features\\tagging.feature");
 formatter.feature({
   "line": 1,
-  "name": "Free CRM Test with DataDriven Using Maps",
+  "name": "Free CRM Application testing",
   "description": "",
-  "id": "free-crm-test-with-datadriven-using-maps",
+  "id": "free-crm-application-testing",
   "keyword": "Feature"
 });
 formatter.scenario({
-  "line": 3,
-  "name": "Create DataDriven testing using Maps",
+  "line": 39,
+  "name": "Closing test",
   "description": "",
-  "id": "free-crm-test-with-datadriven-using-maps;create-datadriven-testing-using-maps",
+  "id": "free-crm-application-testing;closing-test",
   "type": "scenario",
   "keyword": "Scenario"
 });
 formatter.step({
-  "line": 5,
-  "name": "Login to Free CRM",
+  "line": 40,
+  "name": "This is to close the test",
   "keyword": "Given "
 });
-formatter.step({
-  "line": 6,
-  "name": "Enter valid username and password",
-  "rows": [
-    {
-      "cells": [
-        "username",
-        "password"
-      ],
-      "line": 7
-    },
-    {
-      "cells": [
-        "naveendommata",
-        "Welcome@123"
-      ],
-      "line": 8
-    }
-  ],
-  "keyword": "When "
-});
-formatter.step({
-  "line": 9,
-  "name": "Go to deals page , enter datails title , company, and salary",
-  "rows": [
-    {
-      "cells": [
-        "firstname",
-        "lastname",
-        "position"
-      ],
-      "line": 10
-    },
-    {
-      "cells": [
-        "dealspage",
-        "CTS",
-        "50000"
-      ],
-      "line": 11
-    },
-    {
-      "cells": [
-        "newdealspage",
-        "WF",
-        "60000"
-      ],
-      "line": 12
-    },
-    {
-      "cells": [
-        "newdeal",
-        "CG",
-        "20000"
-      ],
-      "line": 13
-    }
-  ],
-  "keyword": "Then "
-});
 formatter.match({
-  "location": "DataDrivenUsingMapTest.Login_to_Free_CRM()"
+  "location": "TaggingTest.This_is_to_close_the_test()"
 });
 formatter.result({
-  "duration": 10342187722,
-  "status": "passed"
-});
-formatter.match({
-  "location": "DataDrivenUsingMapTest.Enter_valid_username_and_password(DataTable)"
-});
-formatter.result({
-  "duration": 8535006307,
-  "status": "passed"
-});
-formatter.match({
-  "location": "DataDrivenUsingMapTest.Go_to_deals_page_enter_datails_title_company_and_salary(DataTable)"
-});
-formatter.result({
-  "duration": 52504701148,
+  "duration": 77801986,
   "status": "passed"
 });
 });
